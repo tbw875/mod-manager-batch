82baabe75e8ae28c8f8f31479c70f8693ba1c1c4
tbw875 <charger875@gmail.com>
2025-02-04 15:33:17 -0800

    Online mod feature

--- mod_manager.bat
+++ mod_manager.bat
@@ -14,10 +14,11 @@ echo Mod Manager
 echo ================
 echo 1. Disable Mods (Move to mods folder)
 echo 2. Enable Mods (Move to paks folder)
+echo 3. Enable Online-Safe mods
 echo 3. Exit
 echo.
 
-set /p choice="Enter your choice (1-3): "
+set /p choice="Enter your choice (1-4): "
 
 if "%choice%"=="1" (
     echo Moving mod files to mods folder...
@@ -56,6 +57,37 @@ if "%choice%"=="2" (
 )
 
 if "%choice%"=="3" (
+    echo Enabling ONLINE PLAY Mods
+
+    set "online_mods=pakchunk99-Mod_SimpleModMenu.pak pakchunk99-Mod_NoUpperRightText_P.pak pakchunk99-Mod_PlayerLimitEdit_P.pak pakchunk999-Mod_SimpleBlueprintLoader_P.pak pakchunk99-Mods_Gunfighter_5_AIO_P.pak"
+
+    for %%F in ("%PAKS_DIR%\*.pak") do (
+        set "filename=%%~nxF"
+        set "is_standard=false"
+        set "is_online_mod=false"
+        
+        :: Check if this is a standard pak file (pakchunk1-Windows.pak to pakchunk23-Windows.pak)
+        for /l %%i in (0,1,23) do (
+            if "!filename!"=="pakchunk%%i-Windows.pak" (
+                set "is_standard=true"
+            )
+        )
+        :: Check if the file is in our allowed list
+        for %%a in (!online_mods!) do (
+            if "!filename!"=="%%a" (
+                set "is_online_mod=true"
+            )
+        )
+        
+        :: If not a standard file, move it to mods folder
+        if "!is_standard!"=="false" (
+            echo Moving !filename!...
+            move "%%F" "%MODS_DIR%"
+        )
+    )
+)
+
+if "%choice%"=="4" (
     echo Goodbye!
     timeout /t 2 >nul
     exit
